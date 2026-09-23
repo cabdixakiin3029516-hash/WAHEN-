585cba44c39af5e82942aff368dae6fdd5338eb2
cabdixakiin3029516-hash <cabdixakiin3029516@gmail.com>
2026-09-23 13:14:59 +0300

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,4 +1,7 @@
 window.WAHEN_SUPABASE = {
   url: 'https://hkmtlyknwsqxuxmvfaqv.supabase.co',
+  anonKey: 'window.WAHEN_SUPABASE = {
+  url: 'https://hkmtlyknwsqxuxmvfaqv.supabase.co',
   anonKey: 'HALKAN_KEY-GAAGA_GELI'
+};'
 };

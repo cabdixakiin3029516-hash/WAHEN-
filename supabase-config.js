c7ab621a548c746915b5eff84f6cc7b098348856
cabdixakiin3029516-hash <cabdixakiin3029516@gmail.com>
2026-09-23 16:55:23 +0300

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,4 +1,15 @@
 window.WAHEN_SUPABASE = {
   url: 'https://hkmtlyknwsqxuxmvfaqv.supabase.co',
-  anonKey: 'sb_publishable_...'
+  anonKey: 'sb_publishable_0JW5GQQnLPwmyRRbNQOBHg_QoBdH01S'
 };
+
+if (!window.supabase) {
+  console.error('WaHeN: Supabase library lama helin.');
+} else {
+  window.wahenDB = window.supabase.createClient(
+    window.WAHEN_SUPABASE.url,
+    window.WAHEN_SUPABASE.anonKey
+  );
+
+  console.log('WaHeN: Supabase connected ✅');
+}
